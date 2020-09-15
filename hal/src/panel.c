2b159b6883c0080b6673a0589c6f977ddbb2f1e6
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-09-15 09:25:17 -0300

    Add panel component base files

---
+++ hal/src/panel.c
@@ -0,0 +1,11 @@
+/*=============================================================================
+ * Author: Nahuel Espinosa <nahue.espinosa@gmail.com>
+ * Date: 2020/09/15
+ *===========================================================================*/
+
+/*=====[Inclusions of function dependencies]=================================*/
+
+#include "panel.h"
+#include "sapi.h"
+
+/*=====[Implementation of public functions]==================================*/
