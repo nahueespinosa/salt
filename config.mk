4a09e5df3ad62363bf0d646b38a884a7d669ba70
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-09-19 11:54:15 -0300

    Update Makefile

--- config.mk
+++ config.mk
@@ -1,24 +1,24 @@
-#MODULES= app lpc_chip_43xx lpc_board_ciaa_edu_4337 sapi rkh bsp
-#DEFINES=CORE_M4 __USE_LPCOPEN __USE_NEWLIB __EDUCIAA_EID__
+PROGRAM_NAME = salt
 
 VERBOSE=n
 OPT=g
 USE_NANO=y
 SEMIHOST=n
 USE_FPU=y
+LOAD_INRAM=n
 
 # Libraries
 USE_LPCOPEN=y
 USE_SAPI=y
 USE_RKH=y
 
-#EXTRA INCLUDE PATHS
+#INCLUDE PATHS
 INCLUDES += -Iapp/inc
 INCLUDES += -Ibsp/inc
 INCLUDES += -Imodel/src-gen
 INCLUDES += -Imodel/src-gen-interface
 
-#EXTRA SOURCE FILES
+#SOURCE FILES
 SRC += $(wildcard app/src/*.c)
 SRC += $(wildcard bsp/src/*.c)
 SRC += $(wildcard model/src-gen/*.c)
